a9b059485b11c19d18c095a6a0be50f0dd23f6a9
Saurabh Dubey <saurabhdubey3712@gmail.com>
2025-07-20 22:10:41 +0530

    connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
 
 
 
 
-(async()=>{})()
+(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        
+    } catch (error) {
+        console.error("ERROR:" , error)
+        throw err
+    }
+})()
